364ca7eb24ef5d4194ac284a94d15dec30bf2655
sylviaessam <sylviaessam@Selveas-MacBook-Pro.local>
2019-08-07 14:37:17 +0200

    last update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,51 +1,124 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   // createPage({
+//   //   path: "/classes/*",
+//   //   matchPath: "/classes/:id",
+//   //   component: path.resolve(`./src/pages/trainers/index.js`),
+//   // })
+//   createPage({
+//     path: "/trainer/*",
+//     matchPath: "/trainer/:id",
+//     component: path.resolve(`src/pages/trainers/index.js`),
+//   })
+//   // createPage({
+//   //   path: "/studio/*",
+//   //   matchPath: "/studio/:id",
+//   //   component: path.resolve(`src/pages/studios/index.js`),
+//   // })
+//   // createPage({
+//   //   path: "/invoice/*",
+//   //   matchPath: "/invoice/",
+//   //   component: path.resolve(`src/pages/booking/index.js`),
+//   // })
+//   // createPage({
+//   //   path: "/myProfile/*",
+//   //   matchPath: "/myProfile/",
+//   //   component: path.resolve(`src/pages/profile/index.js`),
+//   // })
+//   // createPage({
+//   //   path: "/myBooking/*",
+//   //   matchPath: "/myBooking/",
+//   //   component: path.resolve(`src/pages/my_booking/index.js`),
+//   // })
+//   // createPage({
+//   //   path: "/myFav/*",
+//   //   matchPath: "/myFav/",
+//   //   component: path.resolve(`src/pages/my_fav/index.js`),
+//   // })
+//   // createPage({
+//   //   path: "/q&a/*",
+//   //   matchPath: "/q&a/",
+//   //   component: path.resolve(`src/pages/q_a/index.js`),
+//   // })
+
+//   return null
+// }
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  // createPage({
-  //   path: "/classes/*",
-  //   matchPath: "/classes/:id",
-  //   component: path.resolve(`./src/pages/trainers/index.js`),
-  // })
-  createPage({
-    path: "/trainer/*",
-    matchPath: "/trainer/:id",
-    component: path.resolve(`src/pages/trainers/index.js`),
-  })
-  // createPage({
-  //   path: "/studio/*",
-  //   matchPath: "/studio/:id",
-  //   component: path.resolve(`src/pages/studios/index.js`),
-  // })
-  // createPage({
-  //   path: "/invoice/*",
-  //   matchPath: "/invoice/",
-  //   component: path.resolve(`src/pages/booking/index.js`),
-  // })
-  // createPage({
-  //   path: "/myProfile/*",
-  //   matchPath: "/myProfile/",
-  //   component: path.resolve(`src/pages/profile/index.js`),
-  // })
-  // createPage({
-  //   path: "/myBooking/*",
-  //   matchPath: "/myBooking/",
-  //   component: path.resolve(`src/pages/my_booking/index.js`),
-  // })
-  // createPage({
-  //   path: "/myFav/*",
-  //   matchPath: "/myFav/",
-  //   component: path.resolve(`src/pages/my_fav/index.js`),
-  // })
-  // createPage({
-  //   path: "/q&a/*",
-  //   matchPath: "/q&a/",
-  //   component: path.resolve(`src/pages/q_a/index.js`),
-  // })
+  // const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  return graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
 
-  return null
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
+
+    // posts.forEach((post, index) => {
+    //   const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    //   const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: "/studio/*",
+      matchPath: "/studio/:id",
+      component: path.resolve(`src/pages/studios/index.js`),
+    })
+    createPage({
+      path: "/invoice/*",
+      matchPath: "/invoice/",
+      component: path.resolve(`src/pages/booking/index.js`),
+    })
+    createPage({
+      path: "/myProfile/*",
+      matchPath: "/myProfile/",
+      component: path.resolve(`src/pages/profile/index.js`),
+    })
+    createPage({
+      path: "/myBooking/*",
+      matchPath: "/myBooking/",
+      component: path.resolve(`src/pages/my_booking/index.js`),
+    })
+    createPage({
+      path: "/myFav/*",
+      matchPath: "/myFav/",
+      component: path.resolve(`src/pages/my_fav/index.js`),
+    })
+    createPage({
+      path: "/q&a/*",
+      matchPath: "/q&a/",
+      component: path.resolve(`src/pages/q_a/index.js`),
+    })
+
+    // })
+
+    return null
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
