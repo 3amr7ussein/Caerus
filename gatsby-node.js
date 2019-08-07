184852b56880ca27feb383b6e6815923f4d436ce
sylviaessam <sylviaessam@Selveas-MacBook-Pro.local>
2019-08-07 13:52:08 +0200

    last update

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,46 +4,46 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  createPage({
-    path: "/classes/*",
-    matchPath: "/classes/:id",
-    component: path.resolve(`./src/pages/trainers/index.js`),
-  })
-  createPage({
-    path: "/trainer/*",
-    matchPath: "/trainer/:id",
-    component: path.resolve(`src/pages/trainers/index.js`),
-  })
-  createPage({
-    path: "/studio/*",
-    matchPath: "/studio/:id",
-    component: path.resolve(`src/pages/studios/index.js`),
-  })
-  createPage({
-    path: "/invoice/*",
-    matchPath: "/invoice/",
-    component: path.resolve(`src/pages/booking/index.js`),
-  })
-  createPage({
-    path: "/myProfile/*",
-    matchPath: "/myProfile/",
-    component: path.resolve(`src/pages/profile/index.js`),
-  })
-  createPage({
-    path: "/myBooking/*",
-    matchPath: "/myBooking/",
-    component: path.resolve(`src/pages/my_booking/index.js`),
-  })
-  createPage({
-    path: "/myFav/*",
-    matchPath: "/myFav/",
-    component: path.resolve(`src/pages/my_fav/index.js`),
-  })
-  createPage({
-    path: "/q&a/*",
-    matchPath: "/q&a/",
-    component: path.resolve(`src/pages/q_a/index.js`),
-  })
+  // createPage({
+  //   path: "/classes/*",
+  //   matchPath: "/classes/:id",
+  //   component: path.resolve(`./src/pages/trainers/index.js`),
+  // })
+  // createPage({
+  //   path: "/trainer/*",
+  //   matchPath: "/trainer/:id",
+  //   component: path.resolve(`src/pages/trainers/index.js`),
+  // })
+  // createPage({
+  //   path: "/studio/*",
+  //   matchPath: "/studio/:id",
+  //   component: path.resolve(`src/pages/studios/index.js`),
+  // })
+  // createPage({
+  //   path: "/invoice/*",
+  //   matchPath: "/invoice/",
+  //   component: path.resolve(`src/pages/booking/index.js`),
+  // })
+  // createPage({
+  //   path: "/myProfile/*",
+  //   matchPath: "/myProfile/",
+  //   component: path.resolve(`src/pages/profile/index.js`),
+  // })
+  // createPage({
+  //   path: "/myBooking/*",
+  //   matchPath: "/myBooking/",
+  //   component: path.resolve(`src/pages/my_booking/index.js`),
+  // })
+  // createPage({
+  //   path: "/myFav/*",
+  //   matchPath: "/myFav/",
+  //   component: path.resolve(`src/pages/my_fav/index.js`),
+  // })
+  // createPage({
+  //   path: "/q&a/*",
+  //   matchPath: "/q&a/",
+  //   component: path.resolve(`src/pages/q_a/index.js`),
+  // })
 
   return null
 }
